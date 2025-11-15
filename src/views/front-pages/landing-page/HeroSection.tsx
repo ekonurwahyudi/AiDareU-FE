ac5265438779bc6aa2f9065d3e8f0183054fb9f0
Eko Wahyudi <generasic@gmail.com>
2025-11-15 17:41:49 +0700

    update hero

--- src/views/front-pages/landing-page/HeroSection.tsx
+++ src/views/front-pages/landing-page/HeroSection.tsx
@@ -77,9 +77,9 @@ const HeroSection = ({ mode }: { mode: SystemMode }) => {
       <div className={classnames('pbs-[88px] overflow-hidden', frontCommonStyles.layoutSpacing)}>
         <div className='md:max-is-[750px] mbs-0 mli-auto text-center relative'>
           <Typography
-            className={classnames('font-extrabold sm:text-[52px] text-4xl mbe-4 leading-tight', styles.heroText)}
+            className={classnames('font-extrabold sm:text-[48px] text-4xl mbe-4 leading-tight', styles.heroText)}
           >
-            Platformnya Para Entrepreneur untuk Mulai dan Scale Up Bisnis
+            Platformnya Para Entrepreneur untuk Mulai dan Scale Up
           </Typography>
           <Typography className='font-medium text-base sm:text-lg mbe-3' color='text.primary'>
             Jangkau Pelanggan Lebih Luas dengan Website Toko Online Sendiri, Kami siap membantu brand kamu tumbuh di era transformasi digital.
